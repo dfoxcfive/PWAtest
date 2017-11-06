c29348ba3e813764f983f82e94e3985196d5bcc3
thefoxdude <dansparkyfox@gmail.com>
2017-11-05 19:23:59 -0800

    Working on getting a manifest and service worker

--- src/sw.js
+++ src/sw.js
@@ -1,25 +1,64 @@
-// The SW will be shutdown when not in use to save memory,
-// be aware that any global state is likely to disappear
-console.log("SW startup");
+var cacheName = "myapp-cache2";
 
 self.addEventListener('install', function(event) {
   console.log("SW installed");
   event.waitUntil(
-    caches.open('myapp-static-v1').then(function(cache) {
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        '/index.html',
         '/styles.css',
-        '/app/app.module.ts'
+        '/app/app.module.ts',
+        '/manifest.json',
+        '/main.js',
+        '/app/dashboard.component.ts',
+        '/app/my-app-database.ts',
+        '/app/my-app-database.service.js',
+        '/app/heroes.component.ts',
+        '/app/heroes.component.html',
+        '/app/heroes.component.css',
+        '/app/hero.ts',
+        '/app/hero.service.ts',
+        '/app/hero-detail.component.ts',
+        '/app/hero-detail.component.html',
+        '/app/dashboard.component.html',
+        '/app/app.component.ts',
+        '/app/app-routing.module.ts',
+        '/systemjs.config.js',
+        '/tsconfig.json',
+        '/launcher-icon-4x.png',
+        '/node_modules/offline-js/offline.min.js'
       ]);
     })
   )
+  self.skipWaiting()
 });
 
 self.addEventListener('activate', function(event) {
   console.log("SW activated");
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      Promise.all(
+        names.filter(function(name) {
+          return name !== cacheName;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      )
+    })
+  )
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log("Caught a fetch!");
-  
-});
+self.addEventListener('update', function(event) {
+  self.skipWaiting();
+})
+
+// self.addEventListener('fetch', function(event) {
+//   console.log("Caught a fetch!");
+//   event.respondWith(
+//     caches.open(cacheName).then(function(cache) {
+//       // return cache.match(event.request);
+//       console.log(event.request);
+//     })
+//   )
+// });
+
